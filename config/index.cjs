571af5bb9f86fe8534160a94addf8c8a9bd38969
czx-debug <134073093+czx-debug@users.noreply.github.com>
2023-05-19 23:11:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,8 +20,7 @@ const USER_CONFIG = {
     {
       // 想要发送的人的名字
       name: '嫚嫚'',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ohJHM6UkLc_tcbBxcgceqXl1Uhv4',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看
       id: 'ohJHM6cpS5FlN0id0F9jaJv1f2po',
       
       // 使用微信测试号：你想对他发送的模板消息的模板ID
