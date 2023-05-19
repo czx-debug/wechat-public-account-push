6f0ecca504634f0b0bc44cfc7c8e7f6158044064
czx-debug <134073093+czx-debug@users.noreply.github.com>
2023-05-19 22:54:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '嫚嫚'',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohJHM6UkLc_tcbBxcgceqXl1Uhv4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,7 +29,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2001', date: '08-21',
+          type: '*生日', name: '嫚嫚'', year: '2001', date: '08-21',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
