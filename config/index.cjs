e37f46b51baa36ead5e395c7bd2a47acd3dd841c
czx-debug <134073093+czx-debug@users.noreply.github.com>
2023-05-19 22:45:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohJHM6UkLc_tcbBxcgceqXl1Uhv4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'P5eDBz-oHsBsLeC5zjfXx_kcGz7VRnGgdA9DluTT-Xc		',
+      useTemplateId: 'P5eDBz-oHsBsLeC5zjfXx_kcGz7VRnGgdA9DluTT-Xc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-21',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'Awa5F5E0ysoH6GQPH4n1_o1ErjZR6GnhLtMroMpgIEs',
+  CALLBACK_TEMPLATE_ID: 'P5eDBz-oHsBsLeC5zjfXx_kcGz7VRnGgdA9DluTT-Xc',
 
   CALLBACK_USERS: [
     {
